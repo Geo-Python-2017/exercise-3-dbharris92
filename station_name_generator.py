68ec2848a912a5be05739306d1331c2a3a4a4a84
dbharris92 <harris.db92@gmail.com>
2018-02-01 21:06:30 -0500

    first version needs work

---
+++ station_name_generator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 17:44:53 2018
+
+@author: harrisab2
+"""
+
+###variables
+basename = "Station"
+filenames = []
+
+
+### iterate number range 0-20
+for number in range(21):
+    station = (basename,number,'.txt')
+
+
