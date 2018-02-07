a80d732be18e72c7b242bde60201101ce8582e4a
dbharris92 <harris.db92@gmail.com>
2018-02-07 16:53:55 -0500

    progress on problem 3

--- allocate_locations.py
+++ allocate_locations.py
@@ -40,14 +40,14 @@ stations = ['Hanko Russarö', 'Heinola Asemantaus', 'Helsinki Kaisaniemi',
             'Vesanto Sonkari', 'Vieremä Kaarakkala', 'Vihti Maasoja', 'Ylitornio Meltosjärvi']
 
 # Latitude coordinates of Weather stations  
-lat = [59.77, 61.2, 60.18, 60.25, 60.6, 61.88, 63.23, 62.4,
+lats = [59.77, 61.2, 60.18, 60.25, 60.6, 61.88, 63.23, 62.4,
        60.39, 63.12, 65.78, 60.38, 60.7, 60.9, 63.14, 65.99,
        63.32, 63.44, 59.78, 61.47, 66.58, 60.37, 61.8, 62.94,
        64.68, 63.01, 62.24, 70.08, 64.5, 63.06, 62.92, 63.84,
        60.42, 66.53]
 
 # Longitude coordinates of Weather stations 
-lon = [22.95, 26.05, 24.94, 25.05, 24.8, 26.09, 29.23, 25.67, 
+lons = [22.95, 26.05, 24.94, 25.05, 24.8, 26.09, 29.23, 25.67, 
        22.55, 23.04, 24.58, 26.96, 26.81, 26.95, 27.31, 29.23, 
        30.05, 21.07, 21.37, 21.79, 26.01, 23.11, 29.32, 22.49, 
        25.09, 27.8, 30.35, 27.9, 26.42, 21.75, 26.42, 27.22, 
@@ -62,15 +62,19 @@ EWcutoff = 26.3
 # Step 1. Create lists for geographical zones (North-West, North-East, South-West, South-East)
 # ---------------------------------------------------------------------------------------------
 # Add your code here
-
+NW = []
+NE = []
+SW = []
+SE = []
 # Step 2. Make a loop that iterates N number of times. 
+n = len(stations)
 # N should be based on the number of stations that we have here. 
 # --------------------------------------------------------------
-# Add your code here
-    
+for i in range(n):    
     # Step 2.1 - Find out the latitude, longitude and the name of the station
-
-    # Add your code here
+    lat = lats[i]
+    lon = lons[i]        
+    station = stations[i]
 
 
     # Steps 2.2-2.3 - Make conditional statements to add the station to correct zone -list
@@ -79,6 +83,14 @@ EWcutoff = 26.3
     # AND 
     # b) if the lon coordinate is East or West of the center longitude (stored in EWcutoff variable)
     # c) Based on the criteria you should add the NAME of the station (from stations -list) to correct list (done in step 1)
+    if lat < NScutoff:
+        if lon < EWcutoff:
+            SW.append(station)
+        else:
+            SE.append(station)
+        
+        
+        
     
     
     # Add your code here
@@ -88,10 +100,10 @@ EWcutoff = 26.3
 # ----------------------------------------------------------
 
 # Replace the XXX with your list names
-print("The names of the North-West stations are:\n", XXX)
-print("The names of the North-East stations are:\n", XXX)
-print("The names of the South-West stations are:\n", XXX)
-print("The names of the South-East stations are:\n", XXX)
+#print("The names of the North-West stations are:\n", XXX)
+#print("The names of the North-East stations are:\n", XXX)
+#print("The names of the South-West stations are:\n", XXX)
+#print("The names of the South-East stations are:\n", XXX)
 
 # Step 4 (optional) - Print the share of stations at each geographical zone (in percentages)
 # ------------------------------------------------------------------------------------------
